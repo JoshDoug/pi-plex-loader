79bd1c93ad2046bcc5afff37599843e1965db432
Joshua Stringfellow <joshdougstringfellow@gmail.com>
2020-06-14 17:28:34 +0100

    Add default test class

---
+++ loader-api/src/test/java/com/jds/loaderapi/LoaderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jds.loaderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoaderApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
